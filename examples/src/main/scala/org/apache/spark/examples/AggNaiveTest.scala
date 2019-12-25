cd3d11eb95b1ac3d175856d59022555849fd1e29
fluorinedog <fluorinedog@gmail.com>
2019-12-25 19:08:35 +0800

    update

--- examples/src/main/scala/org/apache/spark/examples/AggNaiveTest.scala
+++ examples/src/main/scala/org/apache/spark/examples/AggNaiveTest.scala
@@ -1,28 +1,22 @@
 
 package org.apache.spark.examples
 
-import org.apache.spark.sql.catalyst.expressions.{Descending, SortOrder, Ascending}
-import org.apache.spark.sql.expressions.MutableAggregationBuffer
-import org.apache.spark.sql.expressions.UserDefinedAggregateFunction
-import org.apache.spark.sql.{DataFrame, ExplainMode, Row, SQLContext, SparkSession, SparkSessionExtensions}
+import org.apache.spark.sql.catalyst.expressions.{Ascending, Descending, SortOrder}
 import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, Sort}
 import org.apache.spark.sql.catalyst.rules.Rule
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions._
-import org.apache.zookeeper.KeeperException.UnimplementedException
+import org.apache.spark.sql.{ExplainMode, SparkSession, SparkSessionExtensions}
 
 
 case class ForbiddenDescSorts(spark: SparkSession) extends Rule[LogicalPlan] {
   def apply(plan: LogicalPlan): LogicalPlan = plan transform {
-    case ss@Sort(orders, isGlobalOrder, sortChild) if orders.size == 1 && orders(0).direction == Descending
+    case ss@Sort(orders, isGlobalOrder, sortChild)
+      if orders.size == 1 && orders(0).direction == Descending
     => orders(0) match {
       case SortOrder(orderChild, Descending, no, so)
       => Sort(Seq(SortOrder(orderChild, Ascending, no, so)), isGlobalOrder, sortChild)
     }
   }
 }
-
-//case class FuckParser(spark: SparkSession) extends
 
 // scalastyle:off println
 object AggNaiveTest {
@@ -31,40 +25,24 @@ object AggNaiveTest {
 
     val f: ExtensioniBuilder = _.injectOptimizerRule(ForbiddenDescSorts)
 
-
     val spark = SparkSession
       .builder
       .appName("AggNaive")
-            .withExtensions(f)
+      .config("pa")
+      //      .withExtensions(f)
       .getOrCreate()
+
+    spark.udf.register("fuck", (s: Long) => s * s)
 
     import spark.implicits._
 
-    val data = Seq(
-      ("a", 1, 1),
-      ("b", 1, 2),
-      ("c", 3, 3),
-      ("d", 3, 4),
-      ("a", 1 + 5, 11),
-      ("b", 1 + 5, 12),
-      ("c", 3 + 5, 13),
-      ("d", 3 + 5, 14),
-      ("a", 2, 5),
-      ("b", 2, 6),
-      ("c", 4, 7),
-      ("d", 4, 8),
-      ("a", 2 + 5, 15),
-      ("b", 2 + 5, 16),
-      ("c", 4 + 5, 17),
-      ("d", 4 + 5, 18),
-    )
+    val data = (0 until (1 * 1000 * 1000)).toList.toSeq.flatMap(iterIndex => Seq((iterIndex, 120)))
 
     val df = data
-      .toDF("a", "b", "c")
+      .toDF("b", "c")
 
-    val res = df
-      .select('a, 'b, 'c)
-      .orderBy('c.desc)
+    df.createOrReplaceTempView("tt")
+    val res = spark.sql("select sum(fuck(b) + c) from tt");
 
     res.explain(ExplainMode.Extended)
     val dbgInfo = res.toString()
